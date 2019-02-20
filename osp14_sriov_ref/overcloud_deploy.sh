68c269f3803ced1a75ddeceb6e32cf050b15d177
Jaganathan Palanisamy <jpalanis@redhat.com>
2019-02-20 13:53:49 -0500

    OSP14 ovs dpdk and sriov templates

---
+++ osp14_sriov_ref/overcloud_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PARAMS="$*"
+
+openstack overcloud deploy $PARAMS \
+    --templates \
+    --timeout 120 \
+    -r ~/osp14_ref/roles_data.yaml \
+    -n ~/osp14_ref/network_data_routed_pool3.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+    -e ~/osp14_ref/environment.yaml \
+    -e ~/osp14_ref/network-environment.yaml \
+    -e ~/osp14_ref/ml2-ovs-dpdk-env.yaml \
+    -e ~/containers-prepare-parameter.yaml
+
+#    -e ~/osp14_ref/routed-environment.yaml \
