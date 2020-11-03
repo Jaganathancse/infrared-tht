c30eb185fa3990ef107a27e1cb0f55270c540e9e
Jaganathan Palanisamy <jpalanis@redhat.com>
2020-11-03 14:03:37 -0500

    osp16 templates

---
+++ osp16_ref_mq_perf/overcloud_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PARAMS="$*"
+USER_THT="$HOME/osp16_ref"
+
+if [[ ! -f "$HOME/roles_data.yaml" ]]; then
+	openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeOvsDpdk ComputeSriov
+fi
+
+openstack overcloud deploy $PARAMS \
+    --templates \
+    --timeout 120 \
+    -r $HOME/roles_data.yaml \
+    -n $USER_THT/network_data_routed_pool3.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/netcontrold.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+    -e $USER_THT/environment.yaml \
+    -e $USER_THT/network-environment.yaml \
+    -e $USER_THT/ml2-ovs-dpdk-pmd8.yaml \
+    -e $HOME/containers-prepare-parameter.yaml
+
